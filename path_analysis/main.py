e43589f95d92407f12235409dce3fe39b721ccc4
Dong-Yunan <13370159364@163.com>
2017-05-12 18:48:11 +0800

    test zhuge
    test-zhuge

---
+++ path_analysis/main.py
@@ -0,0 +1,11 @@
+# main.py
+import os
+os.getcwd()
+os.chdir('D:\\Things_List\\ZaiHang\\product_analysis\\py_sth\\fenda\\path_analysis')
+os.getcwd()
+
+#os.popen("D:\\Things_List\\ZaiHang\\product_analysis\\py_sth\\fenda\\path_analysis\\cv_look_1.py")
+
+#os.system("python D:\\Things_List\\ZaiHang\\product_analysis\\py_sth\\fenda\\path_analysis\\cv_look_1.py")
+
+execfile("D:\\Things_List\\ZaiHang\\product_analysis\\py_sth\\fenda\\path_analysis\\cv_look_1.py")
